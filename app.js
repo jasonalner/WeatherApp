465d981da44bacc0bed18e45324dd92f4b0891de
usermane66 <68332445+usermane66@users.noreply.github.com>
2021-03-18 09:26:35 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const button = document.querySelector('.button')
+const inputValue = document.querySelector('.inputValue')
+const name = document.querySelector('.name')
+const desc = document.querySelector('.desc')
+const temp = document.querySelector('.temp')
+
+button.addEventListener('click', function() {
+	fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=metric&appid=f68daa637db78a934561270ec0eff1fc')
+	.then(response => response.json())
+	.then(data => {
+		let nameValue = data['name']
+		let descValue = data['weather'][0]['description']
+		let tempValue = data['main']['temp']
+
+		name.innerHTML = nameValue
+		desc.innerHTML = 'The weather right now is ' + descValue + '.'
+		temp.innerHTML = 'The temperature is ' + tempValue + 'celsius.'
+		console.log(data)
+	})
+
+	.catch(err => alert('Please enter a city name'))
+
+})
